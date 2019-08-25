a2d8d30c0d91bcfe4ecd8db84a883fdb24a7b8d9
Pradeep Kumar <kumarpradeep1010@gmail.com>
2019-08-24 20:55:39 -0500

    [Reverse a String]

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -1,3 +1,4 @@
+
 // --- Directions
 // Given a string, return a new string with the reversed
 // order of characters
@@ -6,6 +7,16 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    return str.split('').reverse().join('');
+}
+
+function reverseV2(str) {
+    let rev = '';
+    for (let char of str) {
+        rev = char + rev;
+    }
+    return rev;
+}
 
 module.exports = reverse;
